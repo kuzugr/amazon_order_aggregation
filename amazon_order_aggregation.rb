807fd04e731f879a7f14f609f1c575ce9d76ce0b
kuzugr <monmon.syusyo@gmail.com>
2019-11-07 10:16:18 +0900

    購入月の表示を月までに変更。コードフォーマットを修正

--- amazon_order_aggregation.rb
+++ amazon_order_aggregation.rb
@@ -12,12 +12,13 @@ class AmazonOrderAggregation
     end
 
     private
+
     def parse_csv_data_by_month
       parsed_datas = {}
       csv_files = Dir.glob('./csvs/*.csv')
       csv_files.each do |csv_file|
         CSV.read(csv_file, headers: true, liberal_parsing: true).each do |data|
-          order_month = Date.strptime(data[0], '%Y/%m')
+          order_month = Date.strptime(data[0], '%Y/%m').strftime('%Y-%m')
           parsed_datas[order_month] = parsed_datas[order_month].to_i + data[4].to_i
         end
       end
@@ -25,9 +26,10 @@ class AmazonOrderAggregation
     end
 
     def price_diff(this_month_price, last_month_price)
-      return 0 if this_month_price == 0 || last_month_price == 0
+      return 0 if this_month_price.zero? || last_month_price.zero?
+
       price_diff = this_month_price - last_month_price
-      if price_diff > 0
+      if price_diff.positive?
         "+#{price_diff}"
       else
         price_diff
